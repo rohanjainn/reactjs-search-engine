2fb22a6103736b34f80a672f858ab967f3b0f467
rohan <iamrohanjainn@gmail.com>
2020-11-16 19:49:50 +0530

    removed log

--- src/components/App.js
+++ src/components/App.js
@@ -6,8 +6,7 @@ import SearchBar from './SearchBar'
 class App extends React.Component{
 
     async onSearchSubmit(term){
-      //  console.log(term);
-
+    
         const response =await axios.get('https://api.unsplash.com/search/photos',{
             params:{query:term},
             headers:{
