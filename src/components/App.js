38ed6be990631c9e08fda57bbedd5469c624c3c3
rohan <iamrohanjainn@gmail.com>
2020-11-22 15:01:10 +0530

    api changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import axios from 'axios';
+import unsplash from '../api/unsplash';
 import SearchBar from './SearchBar'
 
 class App extends React.Component{
 
+    state={images:[]};
+
+    /*
     async onSearchSubmit(term){
     
         const response =await axios.get('https://api.unsplash.com/search/photos',{
@@ -15,11 +18,25 @@ class App extends React.Component{
         });
 
         console.log(response.data.results)
+        console.log(this)
+        this.setState({images:response.data.results})
+    }
+    */
+     onSearchSubmit=async (term)=>{
+    
+        const response =await unsplash.get('/search/photos',{
+            params:{query:term},
+        });
+
+        console.log(response.data.results)
+        console.log(this)
+        this.setState({images:response.data.results})
     }
 
     render(){
         return <div className="ui container" style={{marginTop:'10px'}}>
             <SearchBar runWhenUserSubmit={this.onSearchSubmit}/>
+            Found :{this.state.images.length} images
         </div>
     }
     
