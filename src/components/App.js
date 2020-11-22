2bdf0cf8e564725bcfc136d7ee2bc6386948b11a
rohan <iamrohanjainn@gmail.com>
2020-11-22 18:31:54 +0530

    CSS changes

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import unsplash from '../api/unsplash';
-import SearchBar from './SearchBar'
-
+import SearchBar from './SearchBar';
+import ImageList from './ImageList';
 class App extends React.Component{
 
     state={images:[]};
@@ -36,7 +36,8 @@ class App extends React.Component{
     render(){
         return <div className="ui container" style={{marginTop:'10px'}}>
             <SearchBar runWhenUserSubmit={this.onSearchSubmit}/>
-            Found :{this.state.images.length} images
+            Found :{this.state.images.length} images 
+            <ImageList images={this.state.images}/>
         </div>
     }
     
